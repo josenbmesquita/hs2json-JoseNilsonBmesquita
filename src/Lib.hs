ef8f793a0cb46022c7b57144a683cf34cc4f9fc0
josenbmesquita <jose.n.b.mesquita@gmail.com>
2020-07-07 21:58:53 -0300

    Usos iniciais do Stack

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,13 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+      qsort
     ) where
 
+qsort :: Ord a => [a] -> [a]
+qsort []     = []
+qsort (x:xs) = qsort lhs ++ [x] ++ qsort rhs
+    where lhs = filter  (< x) xs
+          rhs = filter (>= x) xs
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn "Olá mundo!!"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import Test.QuickCheck
+import Lib
+
+prop_idempotent xs = qsort (qsort xs) == qsort xs
+
+return []
+runTests = $quickCheckAll
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = runTests >>= \passed -> if passed then putStrLn "Passou em todos testes."
+                                             else putStrLn "Alguns testes falharam"
